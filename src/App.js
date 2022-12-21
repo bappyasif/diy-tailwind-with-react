3c76a6291c37cbab95f72c659bb5b450e1e32e92
bappyasif <bappyasif@hotmail.com>
2022-12-21 20:38:26 +0600

    initial commit
    * added initial tailwindv3+ installation required by cra
    * tweaked tailwind config a bit to see how that corresponds with ui

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-450 text-primary text-4xl">
+      <h1>Heading Text!!</h1>
+      {/* to see media queries in action they need to be in a container class */}
+      <div className='container bg-slate-600 p-20'>
+        Hallloooo TailwindCSS!!
+        <TailwindCssButton />
+      </div>
+      <TailwindCssButtonReusable>
+        Hoi Hoi!!
+      </TailwindCssButtonReusable>
+    </div>
+  );
+}
+
+const TailwindCssButtonReusable = (props) => {
+  return (
+    <button className='bg-blue-600 text-red-400 px-2 py-.6 font-medium rounded hover:bg-blue-900 inline-block'>
+      {props.children}
+    </button>
+  )
+}
+
+const TailwindCssButton = () => {
+  return (
+    <button className='bg-blue-400 text-red-800 px-2 py-.6 font-semibold rounded hover:bg-blue-900'>
+      Click Here!!
+    </button>
+  )
+}
+
+export default App;
